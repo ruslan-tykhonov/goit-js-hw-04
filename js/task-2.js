28cb5d7086ddfb04d5d62caa2abb5cbdfeda8abf
ruslan-tykhonov <RUSLANEGOROVICH1943@GMAIL.COM>
2024-10-19 19:31:52 +0200

    change

---
+++ js/task-2.js
@@ -0,0 +1,45 @@
+function calcAverageCalories(days) {
+    let totalCaloriesDay = 0;
+    if (days.length) {
+        for (let i = 0; i < days.length; i++ ) {
+            totalCaloriesDay += days[i].calories;
+        }
+        return totalCaloriesDay / days.length
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
